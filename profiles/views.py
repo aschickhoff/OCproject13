21e33417d9002c7ef2f057b3eb0e657bad1b45e0
Andre Schickhoff <achaville@hotmail.de>
2022-08-26 21:38:32 +0200

    updated linting

--- profiles/views.py
+++ profiles/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from .models import Profile
 
 
-'''
-Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat libero
-pulvinar eget. Fusc faucibus, urna quis auctor pharetra, massa dolor cursus neque, quis dictum
-lacus d
-'''
+# Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat libero
+# pulvinar eget. Fusc faucibus, urna quis auctor pharetra, massa dolor cursus neque, quis dictum
+# lacus d
 
 
 def index(request):
@@ -15,12 +13,10 @@ def index(request):
     return render(request, 'profiles/index.html', context)
 
 
-'''
-Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac
-laoreet neque quis, pellentesque dui. Nullam facilisis pharetra vulputate. Sed tincidunt, dolor id
-facilisis fringilla, eros leo tristique lacus, it. Nam aliquam dignissim congue. Pellentesque
-habitant morbi tristique senectus et netus et males
-'''
+# Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac
+# laoreet neque quis, pellentesque dui. Nullam facilisis pharetra vulputate. Sed tincidunt, dolor
+# id facilisis fringilla, eros leo tristique lacus, it. Nam aliquam dignissim congue. Pellentesque
+# habitant morbi tristique senectus et netus et males
 
 
 def profile(request, username):
